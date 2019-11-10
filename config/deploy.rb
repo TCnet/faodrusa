86fdc66c4bd256ed885c781104bf05e03f75f45d
Hxmusic <hxmusic@foxmail.com>
2019-11-10 19:22:39 +0800

    add link_dirs for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :keep_releases, 5
 set :linked_files, %w{config/database.yml config/master.key}
 
 # dirs we want symlinking to shared
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads storage}
 
 
 # what specs should be run before deployment is allowed to
